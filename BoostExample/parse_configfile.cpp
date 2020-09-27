2c89098ab1090ea443d55a30e705419c5049e45f
Gabriel Kabbe <gabriel.kabbe@mail.de>
2020-09-27 16:24:34 +0200

    Add config file example

--- BoostExample/parse_configfile.cpp
+++ BoostExample/parse_configfile.cpp
@@ -1,5 +1,18 @@
 #include <fstream>
+#include <iostream>
+
+#include "config.h"
 
 int main(int argc, char* argv[]) {
   std::ifstream configfile{"config.ini"};
+
+  po::variables_map vm;
+  auto opt = get_options();
+  auto parsed = po::parse_config_file("config.ini", opt, true);
+  po::store(parsed, vm);
+  po::notify(vm);
+
+  for (auto &[a, b] : vm) {
+    std::cout << a << ", " << b.as<std::string>() << std::endl;
+  }
 }
