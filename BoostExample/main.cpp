d6cd501ba2b15ee1368cedd87fbeeead8b7e7394
Gabriel Kabbe <gabriel.kabbe@mail.de>
2020-09-27 11:23:55 +0200

    Add minimal config

--- BoostExample/main.cpp
+++ BoostExample/main.cpp
@@ -5,6 +5,26 @@
 
 namespace po = boost::program_options;
 
-int main() {
+struct Config {
+  std::string project_name;
+  int time_steps;
+  std::string date;
+};
+
+Config load_config(std::string filename) {
+  Config conf;
+  return conf;
+}
+
+int main(int argc, char* argv[]) {
   std::cout << "hi" << std::endl;
+
+  po::variables_map vm;
+  po::options_description config("Config");
+  config.add_options()("project_name", po::value<std::string>(), "Name")(
+      "time_steps", po::value<int>())("date", po::value<std::string>(), "Bla")(
+      "help", "This is the help");
+
+  po::store(po::parse_command_line(argc, argv, config), vm);
+  po::notify(vm);
 }
